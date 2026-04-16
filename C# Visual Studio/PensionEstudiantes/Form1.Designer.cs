ac2dc4656191fa489fe8def3b945de3a516db31b
Alexander Romero Ramírez <L24580028@reynosa.tecnm.mx>
2026-04-16 03:01:18 -0500

    Add files via upload

---
+++ C# Visual Studio/PensionEstudiantes/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PensiónEstudiantes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPromedio = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.lstR = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvAlumnos = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboCategoria);
+            this.groupBox1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.groupBox1.Location = new System.Drawing.Point(26, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(177, 61);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingrese Categoría";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(6, 26);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(165, 27);
+            this.cboCategoria.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtPromedio);
+            this.groupBox2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.groupBox2.Location = new System.Drawing.Point(26, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(177, 63);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ingrese Promedio";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // txtPromedio
+            // 
+            this.txtPromedio.Location = new System.Drawing.Point(8, 26);
+            this.txtPromedio.Name = "txtPromedio";
+            this.txtPromedio.Size = new System.Drawing.Size(163, 27);
+            this.txtPromedio.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 14F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(117, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PENSIÓN DE ESTUDIANTES";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.Font = new System.Drawing.Font("Bahnschrift Condensed", 14F, System.Drawing.FontStyle.Bold);
+            this.btnLimpiar.Location = new System.Drawing.Point(209, 59);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(114, 39);
+            this.btnLimpiar.TabIndex = 3;
+            this.btnLimpiar.Text = "LIMPIAR";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(334, 59);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(114, 39);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.btnProcesar.Font = new System.Drawing.Font("Bahnschrift Condensed", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.Location = new System.Drawing.Point(209, 114);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(239, 62);
+            this.btnProcesar.TabIndex = 5;
+            this.btnProcesar.Text = "PROCESAR";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // lstR
+            // 
+            this.lstR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstR.Location = new System.Drawing.Point(6, 19);
+            this.lstR.Name = "lstR";
+            this.lstR.Size = new System.Drawing.Size(410, 141);
+            this.lstR.TabIndex = 6;
+            this.lstR.Text = "";
+            this.lstR.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lstR);
+            this.groupBox3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.groupBox3.Location = new System.Drawing.Point(26, 182);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(422, 168);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Resumen de Pension";
+            // 
+            // dgvAlumnos
+            // 
+            this.dgvAlumnos.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgvAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlumnos.Location = new System.Drawing.Point(457, 59);
+            this.dgvAlumnos.Name = "dgvAlumnos";
+            this.dgvAlumnos.ReadOnly = true;
+            this.dgvAlumnos.Size = new System.Drawing.Size(344, 291);
+            this.dgvAlumnos.TabIndex = 8;
+            this.dgvAlumnos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAlumnos_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label2.Location = new System.Drawing.Point(577, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "REGISTROS";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(827, 372);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvAlumnos);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Pensión de Estudiantes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPromedio;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.RichTextBox lstR;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dgvAlumnos;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ C# Visual Studio/PensionEstudiantes/Form1.cs
@@ -0,0 +1,177 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PensiónEstudiantes
+{
+    public partial class Form1 : Form
+    {
+        // Ponemos la cadena aquí arriba para que TODOS la puedan usar fácilmente
+        string cadena = "Server=localhost; Port=3306; Database=EscuelaDB; Uid=root; Pwd=Alexander.2006;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Llenar el combo
+            cboCategoria.Items.Add("A");
+            cboCategoria.Items.Add("B");
+            cboCategoria.Items.Add("C");
+            cboCategoria.Items.Add("D");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(cadena))
+                {
+                    con.Open();
+                    this.Text = "Pensión de Estudiantes - CONECTADO A MYSQL";
+                    con.Close();
+                }
+                // Llamamos a cargar la tabla apenas abra el programa
+                CargarTabla();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo conectar a la base de datos: " + ex.Message);
+            }
+        }
+
+        // --- MÉTODO NUEVO PARA MOSTRAR LA TABLA EN EL CUADRO ---
+        void CargarTabla()
+        {
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(cadena))
+                {
+                    string query = "SELECT * FROM Alumnos";
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, con);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    // dgvAlumnos es el nombre del DataGridView en tu diseño
+                    dgvAlumnos.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al llenar el cuadro: " + ex.Message);
+            }
+        }
+
+        void LimpiarControles()
+        {
+            txtPromedio.Clear();
+            cboCategoria.SelectedIndex = -1;
+            cboCategoria.Focus();
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            LimpiarControles();
+            lstR.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Estás seguro de salir?", "Pensión", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (r == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        string Valida()
+        {
+            if (cboCategoria.SelectedIndex == -1) return "Categoría";
+            if (string.IsNullOrEmpty(txtPromedio.Text)) return "Promedio";
+            return "";
+        }
+
+        void Imprimir(double pension, double descuento, double nuevaPension)
+        {
+            lstR.Clear();
+            lstR.AppendText($"La Pensión es: " + pension.ToString("F2") + "\n");
+            lstR.AppendText($"El descuento es: " + descuento.ToString("F2") + "\n");
+            lstR.AppendText($"La Nueva Pensión es: " + nuevaPension.ToString("F2"));
+        }
+
+        void GuardarEnMySQL(string cat, double prom, double pen, double desc, double np)
+        {
+            using (MySqlConnection con = new MySqlConnection(cadena))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "INSERT INTO Alumnos (categoria, promedio, pension, descuento, nueva_pension) VALUES (@cat, @prom, @pen, @desc, @npen)";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@cat", cat);
+                    cmd.Parameters.AddWithValue("@prom", prom);
+                    cmd.Parameters.AddWithValue("@pen", pen);
+                    cmd.Parameters.AddWithValue("@desc", desc);
+                    cmd.Parameters.AddWithValue("@npen", np);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("¡Datos guardados en MySQL!");
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error de base de datos: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            string error = Valida();
+            if (error == "")
+            {
+                try
+                {
+                    Pension objP = new Pension();
+                    objP.categoria = cboCategoria.SelectedIndex;
+                    objP.promedio = double.Parse(txtPromedio.Text);
+
+                    double p = objP.determinaPension();
+                    double d = objP.calculaDescuento();
+                    double np = objP.determinaNuevaPension();
+
+                    Imprimir(p, d, np);
+                    GuardarEnMySQL(cboCategoria.Text, objP.promedio, p, d, np);
+
+                    // IMPORTANTE: Después de guardar, refrescamos el cuadro
+                    CargarTabla();
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show("Por favor, ingrese un número válido en el promedio.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtPromedio.Clear();
+                    txtPromedio.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("El error está en: " + error, "Pensión", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Este es para un botón extra si quieres "refrescar" manualmente
+        private void btnMostrarTodo_Click(object sender, EventArgs e)
+        {
+            CargarTabla();
+        }
+
+        // Otros eventos vacíos que tenías
+        private void dgvAlumnos_CellContentClick(object sender, DataGridViewCellEventArgs e) { }
+        private void groupBox2_Enter(object sender, EventArgs e) { }
+        private void groupBox1_Enter(object sender, EventArgs e) { }
+        private void label1_Click(object sender, EventArgs e) { }
+        private void richTextBox1_TextChanged(object sender, EventArgs e) { }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
